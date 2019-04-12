17faac6921a2a5907c1ec4936fe880d62004bd83
Vaishnave <Vaishnave@VAISHNAVE-PC>
2019-04-12 04:08:25 -0400

    Add project files.

---
+++ ManoharanV_ASSN02/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ManoharanV_ASSN02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumbers = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnGenerateHistory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumbers
+            // 
+            this.txtNumbers.Location = new System.Drawing.Point(55, 40);
+            this.txtNumbers.Name = "txtNumbers";
+            this.txtNumbers.Size = new System.Drawing.Size(355, 20);
+            this.txtNumbers.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(38, 66);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(496, 216);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Date";
+            this.columnHeader1.Width = 99;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Lotto Max Numbers";
+            this.columnHeader2.Width = 161;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Matches";
+            this.columnHeader3.Width = 228;
+            // 
+            // btnGenerateHistory
+            // 
+            this.btnGenerateHistory.Location = new System.Drawing.Point(55, 305);
+            this.btnGenerateHistory.Name = "btnGenerateHistory";
+            this.btnGenerateHistory.Size = new System.Drawing.Size(115, 46);
+            this.btnGenerateHistory.TabIndex = 2;
+            this.btnGenerateHistory.Text = "Generate Lotto History";
+            this.btnGenerateHistory.UseVisualStyleBackColor = true;
+            this.btnGenerateHistory.Click += new System.EventHandler(this.btnGenerateHistory_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 363);
+            this.Controls.Add(this.btnGenerateHistory);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.txtNumbers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumbers;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button btnGenerateHistory;
+    }
+}
+
---
+++ ManoharanV_ASSN02/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace ManoharanV_ASSN02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerateHistory_Click(object sender, EventArgs e)
+        {
+            string input;
+
+            StreamWriter fil = new StreamWriter("lotto.txt");
+
+            Console.Write("Enter your favourite saying: ");
+            input = Console.ReadLine();
+
+            // Write console input to text file saying.txt:
+            fil.WriteLine(input);
+
+            // Close the file	
+            fil.Close();
+
+
+        }
+    }
+}
